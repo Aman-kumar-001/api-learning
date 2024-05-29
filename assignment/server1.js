03ba1c6d2d3a9ee889fa49ea776e01c10059db7f
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-05-30 02:13:46 +0530

    filter by date

--- assignment/server1.js
+++ assignment/server1.js
@@ -3,9 +3,28 @@ const app = express();
 const port = 8000;
 const data = require('./data.js');
 
-app.get('/' , (req,res) =>{
-    res.json(data);
-})
+const convertDateToTimestamp = (dateString) => {
+    const parts = dateString.split('-');
+    const date = new Date(`${parts[2]}-${parts[1]}-${parts[0]}`);
+    return date.getTime();
+};
+
+app.get('/users', (req, res) => {
+    let allUsers = data;
+
+    if (req.query.startDate && req.query.endDate) {
+        const startDate = convertDateToTimestamp(req.query.startDate);
+        const endDate = convertDateToTimestamp(req.query.endDate);
+
+        allUsers = allUsers.filter(user => {
+            const userDate = convertDateToTimestamp(user.date);
+            return userDate >= startDate && userDate <= endDate;
+        });
+    }
+
+    res.json(allUsers);
+});
+
 
 app.listen(port , ()=>{
     console.log(`Server started at ${port}`);
